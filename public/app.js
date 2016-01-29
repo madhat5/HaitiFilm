0805eb37185ae8df171b9a95acb65b9dab58f888
David Malary <davidmalary@gmail.com>
2016-01-28 19:00:36 -0500

    app.js completed

--- public/app.js
+++ public/app.js
@@ -41,14 +41,57 @@ angular.module('MyUsers', []).directive('ngmyusers', function(){
       }; // end of USER GET
 
       // REGISTER USER FORM
+      this.registerForm = function(){
+        self.register = true;
+        self.login = false;
+      };
 
       // CREATE USER, POST
+      this.registerSubmit = function(){
+        self.$http.post('/users', {username: this.registerUsername, password: this.registerPassword}).then(function success(res){
+          // TEST
+          console.log(res.data);
+
+          self.registerUsername = '';
+          self.registerPassword = '';
+          self.loggedIn = true;
+          self.register = false;
+          self.login = false;
+        }, function error(){
+          alert("D'OH! Seems like Bart broke it. Please try again...");
+        });
+      }; // end of USER REGISTER
 
       // LOGIN USER FORM
+      this.loginForm = function(){
+        self.login = true;
+        self.register = false;
+      };
 
       // LOGIN USER
+      this.loginSubmit = function(){
+        self.$http.post('/login', {username: this.loginUsername, password: this.loginPassword}).then(function success(res){
+          // TEST
+          console.log(res);
+
+          self.loggedIn = true;
+          // TEST
+          console.log(self.loggedIn);
+          this.loginUsername = '';
+          this.loginPassword = '';
+          self.getUser();
+        }, function error(){
+          alert("D'OH! Seems like the wrong email and/or password. Please try again...");
+        });
+      }; // end of USER LOGIN
 
       // LOGOUT USER
+      this.logOut = function(){
+        cookies.remove('loggedIn');
+        self.getUser;
+        self.loggedin = false;
+        console.log(Cookies);
+      };
 
     }] // end of controller
   }; // end of return
